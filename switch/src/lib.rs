f80ef1ae3de7376b5e1a766201d2c1a0f81eecd5
guoxbin <guobin@bit-rock.com>
2019-07-18 22:05:57 +0800

    bootnodes-router subcommand

--- switch/src/lib.rs
+++ switch/src/lib.rs
@@ -1,7 +1,8 @@
 pub mod params;
+use substrate_cli::{VersionInfo, error};
 
-
-pub fn run_switch(cmd: params::SwitchCommandCmd) {
+pub fn run_switch(cmd: params::SwitchCommandCmd) -> substrate_cli::error::Result<()> {
     println!("{}", cmd.switch_test.unwrap_or("".to_string()));
 
+    Ok(())
 }
