39e60abd8fae6eb530d3f66fc54bbfa71454f6eb
Chm <chm@yeefoundation.com>
2019-08-13 15:17:57 +0800

    sharding: move srml sharding to dedicated crate for dependency

--- srml/yee-sharding/src/lib.rs
+++ srml/yee-sharding/src/lib.rs
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with YeeChain.  If not, see <https://www.gnu.org/licenses/>.
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
 #[cfg(feature = "std")]
 use {
     serde::Serialize,
