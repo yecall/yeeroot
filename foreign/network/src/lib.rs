c3150834a52ce2905b4bc118c4d1c987d1debc65
guoxbin <guobin@bit-rock.com>
2019-08-20 15:03:58 +0800

    Foreign network

--- foreign/network/src/lib.rs
+++ foreign/network/src/lib.rs
@@ -1,1 +1,21 @@
 
+mod service;
+#[macro_use]
+mod protocol;
+mod util;
+pub mod config;
+pub mod error;
+pub mod message;
+
+pub use service::{Service, NetworkMsg, ExHashT};
+pub use network_libp2p::{
+    identity, multiaddr,
+    ProtocolId, Severity, Multiaddr,
+    NetworkState, NetworkStatePeer, NetworkStateNotConnectedPeer, NetworkStatePeerEndpoint,
+    NodeKeyConfig, Secret, Secp256k1Secret, Ed25519Secret,
+    build_multiaddr, PeerId, PublicKey, IdentifyInfo, IdentifySpecialization, DefaultIdentifySpecialization,
+};
+pub use message::{generic as generic_message};
+pub use error::Error;
+#[doc(hidden)]
+pub use runtime_primitives::traits::Block as BlockT;
