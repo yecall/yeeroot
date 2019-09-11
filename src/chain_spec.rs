88c33f69dd20d29529e61173245ec651359e7b35
dingyouzhen <moses_paul@163.com>
2019-09-11 17:37:12 +0800

    Modify test conditions for grandpa config

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -158,7 +158,7 @@ fn testnet_template_genesis(
             genesis_sharding_count: 4,
         }),
 		grandpa: Some(GrandpaConfig {
-			authorities: initial_authorities.iter().map(|x| (x.2.clone(), 1)).collect(),
+			authorities: vec![],
 		}),
 	}
 }
