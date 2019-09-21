7481cfac03aafb0ab9300baed59983358105f270
Tresdin <48974008+Tresdin-commander@users.noreply.github.com>
2019-09-21 10:34:35 +0800

    Dw/multi mining (#23)
    * add yeeroot miner
    
    * update by yee Code specification
    
    * Submit job
    
    * check proof
    
    * Refactor sharding mod
    
    * check proof for multi
    
    * update License
    
    * Update Cargo
    
    * Provide mine arg to control mining
    
    * update submit job
    
    * Add original_proof log
    
    * Solve pre hash problem
    
    * Adjustment format
    
    * Expose types in pow mod
    
    * Delete unused packages
    
    * update use crate
    
    * adjustment code format
    
    * Update mod.rs
    
    * update Copyright
    
    * Revert "update Copyright"
    
    This reverts commit 552ef1b763cf43310b7069f771c05a64959796d0.
    
    * update code for review
    
    * Add a blank line at the end
    
    * * Cargo lock
    * Log
    * Params

--- switch/src/params.rs
+++ switch/src/params.rs
@@ -52,9 +52,13 @@ pub struct SwitchCommandCmd {
     #[structopt(short = "l", long = "log", value_name = "LOG_PATTERN")]
     pub log: Option<String>,
 
-    ///Specify max connections
-    #[structopt(short = "m", long = "max connections", value_name = "MAX-CONNECTIONS" ,default_value = "100")]
-    pub max_connections: u16,
+    ///Specify miner poll interval
+    #[structopt(long = "job_refresh_interval ", value_name = "INTERVAL" ,default_value = "6000")]
+    pub job_refresh_interval: u64,
+
+    /// start miner
+    #[structopt( long = "mine")]
+    pub mine: bool,
 
 }
 
@@ -64,3 +68,5 @@ impl substrate_cli::GetLogFilter for SwitchCommandCmd {
         self.log.clone()
     }
 }
+
+
