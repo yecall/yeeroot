5d62cac69a92fb8ee0c1cc40c69266f14f3b791b
GB <guobin@bit-rock.com>
2020-07-14 21:58:58 +0800

    crfg rpc (#74)

--- rpc/src/misc/mod.rs
+++ rpc/src/misc/mod.rs
@@ -10,35 +10,176 @@ use serde_json::Value;
 use std::marker::PhantomData;
 use yee_primitives::RecommitRelay;
 use futures::sync::mpsc;
+use parking_lot::RwLock;
+use crfg::CrfgState;
+use std::time::Duration;
 
 #[rpc]
-pub trait MiscApi<Hash> {
-    #[rpc(name = "chain_recommitRelay")]
-    fn recommit_relay_extrinsic(&self, hash: Hash, index: usize) -> errors::Result<()>;
+pub trait MiscApi<Hash, Number> {
+	#[rpc(name = "author_recommitRelay")]
+	fn recommit_relay_extrinsic(&self, hash: Hash, index: usize) -> errors::Result<()>;
+
+	#[rpc(name = "crfg_state")]
+	fn crfg_state(&self) -> errors::Result<Option<types::CrfgState<Hash, Number>>>;
 }
 
-pub struct Misc<Hash> {
-    recommit_relay_sender: Arc<Option<mpsc::UnboundedSender<RecommitRelay<Hash>>>>,
+pub struct Misc<Hash, Number> {
+	recommit_relay_sender: Arc<Option<mpsc::UnboundedSender<RecommitRelay<Hash>>>>,
+	crfg_state: Arc<RwLock<Option<CrfgState<Hash, Number>>>>,
 }
 
-impl<Hash> Misc<Hash> {
-    pub fn new(recommit_relay_sender: Arc<Option<mpsc::UnboundedSender<RecommitRelay<Hash>>>>) -> Self {
-        Self {
-            recommit_relay_sender
-        }
-    }
+impl<Hash, Number> Misc<Hash, Number> {
+	pub fn new(
+		recommit_relay_sender: Arc<Option<mpsc::UnboundedSender<RecommitRelay<Hash>>>>,
+		crfg_state: Arc<RwLock<Option<CrfgState<Hash, Number>>>>,
+	) -> Self {
+		Self {
+			recommit_relay_sender,
+			crfg_state,
+		}
+	}
 }
 
-impl<Hash> MiscApi<Hash> for Misc<Hash> where
-    Hash: Send + Sync + 'static,
+impl<Hash, Number> MiscApi<Hash, Number> for Misc<Hash, Number> where
+	Hash: Send + Clone + Sync + 'static,
+	Number: Send + Clone + Sync + 'static,
 {
-    fn recommit_relay_extrinsic(&self, hash: Hash, index: usize)  -> errors::Result<()> {
-        let recommit_param = RecommitRelay {
-            hash,
-            index
+	fn recommit_relay_extrinsic(&self, hash: Hash, index: usize) -> errors::Result<()> {
+		let recommit_param = RecommitRelay {
+			hash,
+			index,
         };
-        let sender = self.recommit_relay_sender.as_ref();
-        sender.as_ref().unwrap().unbounded_send(recommit_param);
-        Ok(())
-    }
+		let sender = self.recommit_relay_sender.as_ref();
+		sender.as_ref().unwrap().unbounded_send(recommit_param);
+		Ok(())
+	}
+
+	fn crfg_state(&self) -> errors::Result<Option<types::CrfgState<Hash, Number>>> {
+		let state = self.crfg_state.read().as_ref().cloned().map(|x|x.into());
+		Ok(state)
+	}
 }
+
+mod types {
+	use std::sync::Arc;
+	use yee_runtime::{AuthorityId, BlockNumber};
+	use std::time::Duration;
+	use parity_codec::alloc::collections::HashMap;
+	use serde::Serialize;
+	use yee_serde_hex::SerdeHex;
+	use substrate_primitives::crypto::Pair;
+
+	#[derive(Serialize, Eq, PartialEq, Hash)]
+	pub struct Public(#[serde(with = "SerdeHex")] Vec<u8>);
+
+	#[derive(Serialize)]
+	pub struct CrfgState<H, N> {
+		pub config: Config,
+		pub set_id: u64,
+		pub voters: VoterSet,
+		pub set_status: VoterSetState<H, N>,
+	}
+
+	#[derive(Serialize)]
+	pub struct Config {
+		pub gossip_duration: Duration,
+		pub justification_period: u64,
+		pub local_key_public: Option<Public>,
+		pub local_next_key_public: Option<Public>,
+		pub name: Option<String>,
+	}
+
+	#[derive(Serialize)]
+	pub struct VoterInfo {
+		canon_idx: usize,
+		weight: u64,
+	}
+
+	#[derive(Serialize)]
+	pub struct VoterSet {
+		weights: HashMap<Public, VoterInfo>,
+		voters: Vec<(Public, u64)>,
+		threshold: u64,
+	}
+
+	#[derive(Serialize)]
+	pub enum VoterSetState<H, N> {
+		Paused(u64, RoundState<H, N>),
+		Live(u64, RoundState<H, N>),
+	}
+
+	#[derive(Serialize)]
+	pub struct RoundState<H, N> {
+		pub prevote_ghost: Option<(H, N)>,
+		pub finalized: Option<(H, N)>,
+		pub estimate: Option<(H, N)>,
+		pub completable: bool,
+	}
+
+	impl<H, N> From<crfg::CrfgState<H, N>> for CrfgState<H, N> where
+		H: Clone,
+		N: Clone,
+	{
+		fn from(t: crfg::CrfgState<H, N>) -> CrfgState<H, N> {
+			CrfgState {
+				config: t.config.into(),
+				set_id: t.set_id,
+				voters: t.voters.into(),
+				set_status: t.set_status.into(),
+			}
+		}
+	}
+
+	impl From<crfg::Config> for Config {
+		fn from(t: crfg::Config) -> Config {
+			Config {
+				gossip_duration: t.gossip_duration,
+				justification_period: t.justification_period,
+				local_key_public: t.local_key.map(|x| Public(x.public().0.to_vec())),
+				local_next_key_public: t.local_next_key.map(|x| Public(x.public().0.to_vec())),
+				name: t.name,
+			}
+		}
+	}
+
+	impl From<Arc<grandpa::VoterSet<AuthorityId>>> for VoterSet {
+		fn from(t: Arc<grandpa::VoterSet<AuthorityId>>) -> VoterSet {
+			VoterSet {
+				weights: t.weights.iter().map(|(k, v)| (Public(k.0.to_vec()), v.clone().into())).collect(),
+				voters: t.voters.iter().map(|(l, r)| (Public(l.0.to_vec()), *r)).collect(),
+				threshold: t.threshold,
+			}
+		}
+	}
+
+	impl From<grandpa::VoterInfo> for VoterInfo {
+		fn from(t: grandpa::VoterInfo) -> VoterInfo {
+			VoterInfo {
+				canon_idx: t.canon_idx(),
+				weight: t.weight(),
+			}
+		}
+	}
+
+	impl<H, N> From<crfg::VoterSetState<H, N>> for VoterSetState<H, N> {
+		fn from(t: crfg::VoterSetState<H, N>) -> VoterSetState<H, N> {
+			match t {
+				crfg::VoterSetState::Paused(id, round_state) => VoterSetState::Paused(id, round_state.into()),
+				crfg::VoterSetState::Live(id, round_state) => VoterSetState::Live(id, round_state.into()),
+			}
+		}
+	}
+
+	impl<H, N> From<grandpa::State<H, N>> for RoundState<H, N> {
+		fn from(t: grandpa::State<H, N>) -> RoundState<H, N> {
+			RoundState {
+				prevote_ghost: t.prevote_ghost,
+				finalized: t.finalized,
+				estimate: t.estimate,
+				completable: t.completable,
+			}
+		}
+	}
+}
+
+
