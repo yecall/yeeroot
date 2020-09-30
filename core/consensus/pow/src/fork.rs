e13ec7685f4977cc97eddbdc6ed9288e1fac980a
MayNail <249033876@qq.com>
2020-10-01 01:18:53 +0800

    Update fork conf 2

--- core/consensus/pow/src/fork.rs
+++ core/consensus/pow/src/fork.rs
@@ -17,8 +17,8 @@
 
 /// (chain_spec_id, shard_num) => (block_number, block_hash, fork_id)
 pub const FORK_CONF : [((&str, u16), (u64, &str, u32)); 4] = [
-    (("mainnet", 0),  ( 144005, "0x3c6a4cbfd3e178967aa389db39d8299078471a9a02285a159d78be30334e799e", 1)),
-    (("mainnet", 1),  ( 144063, "0x7bdb8b504814ecba70a6d62e312d37ed1c4c08e31cff83d0c6eea81468fde7de", 1)),
-    (("mainnet", 2),  ( 144052, "0xfbee5fa7822dfef5326aba0cc7e9e735e7e2520e81f5c64e86b6e28007162a16", 1)),
-    (("mainnet", 3),  ( 142548, "0xf79d80730adae46fa1c31893554d2662f83b2ea886bd0df0b406612504728c45", 1)),
+    (("mainnet", 0),  ( 143459, "0x473de2d82aebbefe0e96888d1fde3a7c206f6842aaaafc546fecac4a62f760e1", 1)),
+    (("mainnet", 1),  ( 143514, "0x55867d252b5c8e3eaa54b1ea865e7db035adcb3b6cda8b0fce0a4964bd8a0469", 1)),
+    (("mainnet", 2),  ( 143501, "0x3f39ed1bce2eb79712d05ad799349a66bedc634d764bbed83e800594e3a0f692", 1)),
+    (("mainnet", 3),  ( 141721, "0xee73f763ef9f55a7e85d118026ce62a26597a9c246c914e26a29309242e8c065", 1)),
 ];
